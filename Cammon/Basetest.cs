a9a96b04933987188d09f5caab01d7ab047fd72b
Rodolfo <farleyrodolfo@gmail.com>
2020-04-15 15:57:06 -0300

    first commit

--- Cammon/Basetest.cs
+++ Cammon/Basetest.cs
@@ -3,6 +3,7 @@ using Coypu.Drivers.Selenium;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace google_net.Cammon
@@ -18,7 +19,7 @@ namespace google_net.Cammon
             {
                 Driver = typeof(CustomChromeHeadlessWebDriver),
                 Browser = Coypu.Drivers.Browser.Chrome,
-               // AppHost = "https://specflow.org/", // whatever url you want
+                // AppHost = "https://specflow.org/", // whatever url you want
                 Timeout = TimeSpan.FromSeconds(2)
             };
             Browser = new BrowserSession(sessionConfigurationForChromeHeadless);
@@ -26,11 +27,41 @@ namespace google_net.Cammon
 
         }
 
+        public void TakeScreenshot()
+        {
+            var resultId = TestContext.CurrentContext.Test.ID;
+
+            var shortpath = Environment.CurrentDirectory + "\\Screenshots";
+
+            if (!Directory.Exists(shortpath))
+            {
+                Directory.CreateDirectory(shortpath);
+            }
+
+            var screenshots = $"{shortpath}\\{resultId}.png";
+
+            Browser.SaveScreenshot(screenshots);
+            TestContext.AddTestAttachment(screenshots);
+        }
+
+
         [TearDown]
         public void Finish()
         {
-            Browser.Dispose();
-        }
+            try
+            {
+                TakeScreenshot();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro ao captura imagem ;(");
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                Browser.Dispose();
+            }
 
         }
     }
+ }
